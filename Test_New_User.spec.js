c42dbf35c012c2a119812a18bd7840dc9c64e0a2
wscorrea8 <wscorrea8@misena.edu.co>
2022-03-25 10:42:17 -0500

    Probando nuevas lineas de comando para insertar la ciudad

--- Test_New_User.spec.js
+++ Test_New_User.spec.js
@@ -1,4 +1,21 @@
 describe('Pruebas Pagina de inicio ', function(){
 
+it('Validación registro Usuario', function(){
+
+        cy.visit('https://www.utest.com/')
+        cy.get('.unauthenticated-nav-bar__sign-up').click()
+        cy.get('#firstName').type('william')
+        cy.get('#lastName').type('correa')
+        cy.get('#email').type('sercorrea@gmail.com')
+        cy.get('#birthMonth').select('May')
+        cy.get('#birthDay').select(15)
+        cy.get('#birthYear').select(27)
+        cy.get('.text-right > .btn').click()
+        cy.get('#city').click()
+        cy.get('#city').type('Pereira, Risaralda, Colombia')
+        cy.focused().click()
+        //cy.get('#city').contains('Pereira,Risaralda,Colombia').click()
+        cy.get('#zip').type(66001)
 
   })
+ })
